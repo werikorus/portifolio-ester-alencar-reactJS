932ce0a4988db748e01d4017a3ea11e232b66a7b
Werik Santos <werik.msti@gmail.com>
2021-05-26 23:35:52 -0300

    melhorias

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -15,10 +15,22 @@ import pub1 from './../../assets/images/publicidade/pub1.svg';
 import pub2 from './../../assets/images/publicidade/pub2.svg';
 import pub3 from './../../assets/images/publicidade/pub3.svg';
 
+
+import indV1 from  './../../assets/images/indVisual/ind1.svg';
+import indV2 from './../../assets/images/indVisual/ind1.svg';
+import indV3 from './../../assets/images/indVisual/ind1.svg';
+
+
+import Ui1 from './../../assets/images/UIDesign/rec1.svg';
+import Ui2 from './../../assets/images/UIDesign/rec2.svg';
+import Ui3 from  './../../assets/images/UIDesign/rec1.svg';
+
+
 const edicoes = new Array(edt3, edt2, edt3);
 const cartoes = new Array(cart1, cart2, cart3);
 const publicidade = new Array(pub1, pub2, pub3);
-
+const idVisual = new Array(indV1, indV2, indV3);
+const  uiDesign = new Array(Ui1, Ui2, Ui3);
 
 const MainPage = () =>{
   return (
@@ -26,8 +38,8 @@ const MainPage = () =>{
       <ImagesCarroucel title="Edições" imgs={edicoes}/>
       <ImagesCarroucel title="Cartões" imgs={cartoes}/>
       <ImagesCarroucel title="Publicidade" imgs={publicidade}/>
-      {/* <ImagesCarroucel title={indVisual[0]} imgs={indVisual}/>
-      <ImagesCarroucel title={uiDesign[0]} imgs={uiDesign}/> */}
+      <ImagesCarroucel title="Identidade Visual" imgs={idVisual}/>
+      <ImagesCarroucel title="UI Design" imgs={uiDesign}/>
       
     </main>
   );
